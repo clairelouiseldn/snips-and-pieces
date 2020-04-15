612506e8771cd30fcd1f0957ae3ec43fb4f37948
clairelouiseldn <38511239+clairelouiseldn@users.noreply.github.com>
2020-04-15 18:26:48 +0100

    Update gatsby-config.js
    Update siteMetadata title and description.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Snips and Pieces',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Code snippets for you to learn, edit and use in your own projects.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
